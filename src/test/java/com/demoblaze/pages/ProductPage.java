c0f0cfb57ed14e83782325600fc7769a7330b2de
OscarSah <oscar@cybertekschool.com>
2022-06-21 21:46:54 +0300

    Some steps ready

---
+++ src/test/java/com/demoblaze/pages/ProductPage.java
@@ -0,0 +1,7 @@
+package com.demoblaze.pages;
+
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage extends BasePage{
+
+}
--- src/test/java/com/demoblaze/step_definitions/TestStepDefs.java
+++ src/test/java/com/demoblaze/step_definitions/TestStepDefs.java
@@ -1,17 +1,22 @@
 package com.demoblaze.step_definitions;
 
+import com.demoblaze.pages.ProductPage;
+import com.demoblaze.utilities.ConfigurationReader;
+import com.demoblaze.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
 public class TestStepDefs {
+    ProductPage productPage = new ProductPage();
     @Given("buyer is on the homepage of demoblaze")
     public void buyer_is_on_the_homepage_of_demoblaze() {
-
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
     }
     @When("buyer adds from {string} category {string} product to cart")
     public void buyer_adds_from_category_product_to_cart(String category, String product) {
-
+        productPage.getLink(category);
+        productPage.getLink(product);
     }
     @And("buyer clicks on {string} link")
     public void buyer_clicks_on_link(String link) {
