49f30cd64a54c567d970af11f3f45a5dacb040e2
OscarSah <oscar@cybertekschool.com>
2022-06-21 21:21:06 +0300

    Scenario 1

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -1,4 +1,18 @@
 package com.demoblaze.runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html=target/cucumber-report.html"
+        },
+        features ="src/test/resources/features",
+        glue = "com/demoblaze/step_definitions",
+        dryRun = true,
+        tags = ""
+)
 public class CukesRunner {
 }
--- src/test/resources/features/test.feature
+++ src/test/resources/features/test.feature
@@ -1,1 +1,5 @@
-Feature:
+Feature: Purchase Functionality of Demoblaze Application
+  As a buyer, I should be able navigate through products
+  so that I can purchase from application online
+
+  Scenario: Adding product to cart
