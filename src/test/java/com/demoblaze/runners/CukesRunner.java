9f4fd030a765a51538cfc7420932f8a8e7aed63f
OscarSah <oscar@cybertekschool.com>
2022-06-21 21:56:20 +0300

    Some steps ready

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         },
         features ="src/test/resources/features",
         glue = "com/demoblaze/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@wip"
 )
 public class CukesRunner {
--- src/test/java/com/demoblaze/step_definitions/TestStepDefs.java
+++ src/test/java/com/demoblaze/step_definitions/TestStepDefs.java
@@ -1,11 +1,15 @@
 package com.demoblaze.step_definitions;
 
 import com.demoblaze.pages.ProductPage;
+import com.demoblaze.utilities.BrowserUtils;
 import com.demoblaze.utilities.ConfigurationReader;
 import com.demoblaze.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestStepDefs {
     ProductPage productPage = new ProductPage();
@@ -17,9 +21,16 @@ public class TestStepDefs {
     public void buyer_adds_from_category_product_to_cart(String category, String product) {
         productPage.getLink(category);
         productPage.getLink(product);
+        productPage.getLink("Add to cart");
+        // handle alert
+       // BrowserUtils.sleep(1); static wait
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = Driver.getDriver().switchTo().alert();
+        alert.accept();
     }
     @And("buyer clicks on {string} link")
     public void buyer_clicks_on_link(String link) {
-
+        productPage.getLink(link);
     }
 }
