d3a53768fb898ec2ddd559ddff24e5388abd6e10
OscarSah <oscar@cybertekschool.com>
2022-06-28 21:22:24 +0300

    Cucumber Reporting

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/cucumber-report.html"
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
         },
         features ="src/test/resources/features",
         glue = "com/demoblaze/step_definitions",
